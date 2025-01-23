4fe7e81967a66cc4bc1a00b1b3e62bf1300b2adf
jotvs <joao.vslima@hotmail.com>
2025-01-22 21:44:09 -0300

    Get Game By Id

--- src/main/java/com/jotvs/dslist/services/GameService.java
+++ src/main/java/com/jotvs/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.jotvs.dslist.services;
 
+import com.jotvs.dslist.DTO.GameDTO;
 import com.jotvs.dslist.DTO.GameMinDTO;
 import com.jotvs.dslist.entities.Game;
 import com.jotvs.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,10 +16,17 @@ public class GameService {
     //Vamos utilizar o gameRepository para fazer as consultas no BD
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional (readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         //Transformamos oq é Game para GameMinDTO
         return result.stream().map(GameMinDTO::new).toList();
+    }
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
     }
 }
