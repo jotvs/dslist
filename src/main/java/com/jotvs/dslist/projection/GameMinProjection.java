fbb01b3985b49a987095ed9d88f4e299ba625ce5
jotvs <joao.vslima@hotmail.com>
2025-01-24 22:41:59 -0300

    Homolog ok

--- src/main/java/com/jotvs/dslist/projection/GameMinProjection.java
+++ src/main/java/com/jotvs/dslist/projection/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.jotvs.dslist.projection;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer position ();
